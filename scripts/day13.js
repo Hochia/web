7aae582dc7e47616086ad89fa6550343b1dbc88c
Hochia <johnny30618@gmail.com>
2020-03-03 10:15:35 +0800

    Add day13.js
    -interact with HTML elements
    -available to click on your keyboard directly

---
+++ scripts/day13.js
@@ -0,0 +1,243 @@
+window.addEventListener("keydown", function (event) {
+    var x = event.which;
+    if (String.fromCharCode(x) == "a" || String.fromCharCode(x) == "A") {
+        document.getElementsByClassName("key_a")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "a";
+    } else
+    if (String.fromCharCode(x) == "b" || String.fromCharCode(x) == "B") {
+        document.getElementsByClassName("key_b")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "b";
+    } else
+    if (String.fromCharCode(x) == "c" || String.fromCharCode(x) == "C") {
+        document.getElementsByClassName("key_c")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "c";
+    } else
+    if (String.fromCharCode(x) == "d" || String.fromCharCode(x) == "D") {
+        document.getElementsByClassName("key_d")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "d";
+    } else
+    if (String.fromCharCode(x) == "e" || String.fromCharCode(x) == "E") {
+        document.getElementsByClassName("key_e")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "e";
+    } else
+    if (String.fromCharCode(x) == "f" || String.fromCharCode(x) == "F") {
+        document.getElementsByClassName("key_f")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "f";
+    } else
+    if (String.fromCharCode(x) == "g" || String.fromCharCode(x) == "G") {
+        document.getElementsByClassName("key_g")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "g";
+    } else
+    if (String.fromCharCode(x) == "h" || String.fromCharCode(x) == "H") {
+        document.getElementsByClassName("key_h")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "h";
+    } else
+    if (String.fromCharCode(x) == "i" || String.fromCharCode(x) == "I") {
+        document.getElementsByClassName("key_i")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "i";
+    } else
+    if (String.fromCharCode(x) == "j" || String.fromCharCode(x) == "J") {
+        document.getElementsByClassName("key_j")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "j";
+    } else
+    if (String.fromCharCode(x) == "k" || String.fromCharCode(x) == "K") {
+        document.getElementsByClassName("key_k")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "k";
+    } else
+    if (String.fromCharCode(x) == "l" || String.fromCharCode(x) == "L") {
+        document.getElementsByClassName("key_l")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "L";
+    } else
+    if (String.fromCharCode(x) == "m" || String.fromCharCode(x) == "M") {
+        document.getElementsByClassName("key_m")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "m";
+    } else
+    if (String.fromCharCode(x) == "n" || String.fromCharCode(x) == "N") {
+        document.getElementsByClassName("key_n")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "n";
+    } else
+    if (String.fromCharCode(x) == "o" || String.fromCharCode(x) == "O") {
+        document.getElementsByClassName("key_o")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "o";
+    } else
+    if (String.fromCharCode(x) == "p" || String.fromCharCode(x) == "P") {
+        document.getElementsByClassName("key_p")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "p";
+    } else
+    if (String.fromCharCode(x) == "q" || String.fromCharCode(x) == "Q") {
+        document.getElementsByClassName("key_q")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "q";
+    } else
+    if (String.fromCharCode(x) == "r" || String.fromCharCode(x) == "R") {
+        document.getElementsByClassName("key_r")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "r";
+    } else
+    if (String.fromCharCode(x) == "s" || String.fromCharCode(x) == "S") {
+        document.getElementsByClassName("key_s")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "S";
+    } else
+    if (String.fromCharCode(x) == "t" || String.fromCharCode(x) == "T") {
+        document.getElementsByClassName("key_t")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "t";
+    } else
+    if (String.fromCharCode(x) == "u" || String.fromCharCode(x) == "U") {
+        document.getElementsByClassName("key_u")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "u";
+    } else
+    if (String.fromCharCode(x) == "v" || String.fromCharCode(x) == "V") {
+        document.getElementsByClassName("key_v")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "v";
+    } else
+    if (String.fromCharCode(x) == "w" || String.fromCharCode(x) == "W") {
+        document.getElementsByClassName("key_w")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "w";
+    } else
+    if (String.fromCharCode(x) == "x" || String.fromCharCode(x) == "X") {
+        document.getElementsByClassName("key_x")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "x";
+    } else
+    if (String.fromCharCode(x) == "y" || String.fromCharCode(x) == "Y") {
+        document.getElementsByClassName("key_y")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "y";
+    } else
+    if (String.fromCharCode(x) == "z" || String.fromCharCode(x) == "Z") {
+        document.getElementsByClassName("key_z")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + "z";
+    } else
+    if (String.fromCharCode(x) == " ") {
+        document.getElementsByClassName("whitespace")[0].style.animation = "bd-col 500ms ease-in-out 0ms infinite normal, bg-col 500ms ease-in-out 0ms infinite normal";
+        var shana = document.getElementById("bella").value;
+        document.getElementById("bella").value = shana + " ";
+    }
+})
+
+window.addEventListener("keyup", function (event) {
+    var x = event.which;
+    if (String.fromCharCode(x) == "a" || String.fromCharCode(x) == "A") {
+        document.getElementsByClassName("key_a")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "b" || String.fromCharCode(x) == "B") {
+        document.getElementsByClassName("key_b")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "c" || String.fromCharCode(x) == "C") {
+        document.getElementsByClassName("key_c")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "d" || String.fromCharCode(x) == "D") {
+        document.getElementsByClassName("key_d")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "e" || String.fromCharCode(x) == "E") {
+        document.getElementsByClassName("key_e")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "f" || String.fromCharCode(x) == "F") {
+        document.getElementsByClassName("key_f")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "g" || String.fromCharCode(x) == "G") {
+        document.getElementsByClassName("key_g")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "h" || String.fromCharCode(x) == "H") {
+        document.getElementsByClassName("key_h")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "i" || String.fromCharCode(x) == "I") {
+        document.getElementsByClassName("key_i")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "j" || String.fromCharCode(x) == "J") {
+        document.getElementsByClassName("key_j")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "k" || String.fromCharCode(x) == "K") {
+        document.getElementsByClassName("key_k")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "l" || String.fromCharCode(x) == "L") {
+        document.getElementsByClassName("key_l")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "m" || String.fromCharCode(x) == "M") {
+        document.getElementsByClassName("key_m")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "n" || String.fromCharCode(x) == "N") {
+        document.getElementsByClassName("key_n")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "o" || String.fromCharCode(x) == "O") {
+        document.getElementsByClassName("key_o")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "p" || String.fromCharCode(x) == "P") {
+        document.getElementsByClassName("key_p")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "q" || String.fromCharCode(x) == "Q") {
+        document.getElementsByClassName("key_q")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "r" || String.fromCharCode(x) == "R") {
+        document.getElementsByClassName("key_r")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "s" || String.fromCharCode(x) == "S") {
+        document.getElementsByClassName("key_s")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "t" || String.fromCharCode(x) == "T") {
+        document.getElementsByClassName("key_t")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "u" || String.fromCharCode(x) == "U") {
+        document.getElementsByClassName("key_u")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "v" || String.fromCharCode(x) == "V") {
+        document.getElementsByClassName("key_v")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "w" || String.fromCharCode(x) == "W") {
+        document.getElementsByClassName("key_w")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "x" || String.fromCharCode(x) == "X") {
+        document.getElementsByClassName("key_x")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "y" || String.fromCharCode(x) == "Y") {
+        document.getElementsByClassName("key_y")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == "z" || String.fromCharCode(x) == "Z") {
+        document.getElementsByClassName("key_z")[0].style.animation = "";
+    } else
+    if (String.fromCharCode(x) == " ") {
+        document.getElementsByClassName("whitespace")[0].style.animation = "";
+    } else
+    if (x == 13) {
+        document.getElementsByClassName("right_enter")[0].style.animation = "";
+        congradulate();
+    } else
+    if (x == 8) {
+        document.getElementsByClassName("right_backspace")[0].style.animation = "";
+        typing_right_backspace();
+    }
+})
+function congradulate() {
+    var shana = document.getElementById("bella").value;
+    if (shana.slice(18, 27) == "Shana Liu") {
+        document.getElementById("demo").innerHTML = shana.slice(18, 27) + " is beautiful!";
+    } else {
+        document.getElementById("demo").innerHTML = "女兒節快樂";
+    }
+}
+function typing_right_backspace() {
+    var shana = document.getElementById("bella").value;
+    document.getElementById("bella").value = shana.slice(0, shana.length - 1);
+}
